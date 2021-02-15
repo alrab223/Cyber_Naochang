f210211b2f3cd6819140e2f22807e57d3f5913dc
allbear <allbear826@gmail.com>
2021-02-15 23:18:36 +0900

    不要なファイルを削除

--- db/tes.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-import json
-with (sqlite3.connect("bot_data.db")) as conn:
-    c = conn.cursor()
-    sql = c.execute(f'select id,members,reset,vc_notification from vc_notification_setting where members>0')
-    id_list = [x for x in sql]
-    print(id_list)
- 
--- picture/tes.py
+++
@@ -1,8 +0,0 @@
-import os,glob,shutil
-path="24hour"
-files = os.listdir("24hour")
-files_dir = [int(f) for f in files if os.path.isdir(os.path.join(path, f))]
-sort_file = sorted(files_dir)
-file_list = glob.glob(f"24hour/{sort_file[0]}/*.txt")
-print(file_list)
-shutil.rmtree(f"24hour/{sort_file[22]}")
