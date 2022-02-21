9bd94e972a3d6aeb7151ef564212479bd73558ee
allbear <allbear826@gmail.com>
2022-02-22 00:31:08 +0900

    ライブラリを整理

--- cog/funny_cog.py
+++ cog/funny_cog.py
@@ -1,13 +1,17 @@
 import glob
 import json
 import os
+import random
 
 import discord
+import requests
 from discord.ext import commands
+from discord_components import Button, ButtonStyle, DiscordComponents, Select, SelectOption
 from PIL import Image
+
 from cog.utils import picture_download as pd
-from cog.utils.DbModule import DbModule as db
 from cog.utils import wiki_search
+from cog.utils.DbModule import DbModule as db
 from cog.utils.webhook_control import Webhook_Control
 
 
@@ -16,6 +20,7 @@ class Funny(commands.Cog):
    def __init__(self, bot):
       self.bot = bot
       self.db = db()
+      DiscordComponents(self.bot)
       with open("json/picture.json", "r") as f:
          self.colla_num = json.load(f)
 
@@ -69,6 +74,52 @@ class Funny(commands.Cog):
       await webhook.send(content=emoji,
                          username=ctx.author.display_name,
                          avatar_url=ctx.author.avatar_url_as(format="png"))
+   
+   def quickpick_process(self, ticket: str, starters: int):
+      horse = range(1, starters + 1)
+      if ticket == "単勝,複勝":
+         vote = random.sample(horse, 1)
+      elif ticket == "馬連":
+         vote = random.sample(horse, 2)
+         vote.sort()
+      elif ticket == "馬単":
+         vote = random.sample(horse, 2)
+      elif ticket == "ワイド":
+         vote = random.sample(horse, 2)
+         vote.sort()
+      elif ticket == "3連複":
+         vote = random.sample(horse, 3)
+         vote.sort()
+      else:
+         vote = random.sample(horse, 3)
+      vote = [str(i) for i in vote]
+      vote = "→".join(vote)
+      return vote
+   
+   @ commands.command("クイックピック")
+   async def quickpick(self, ctx, starters: int):
+      comp = []
+      comp2 = []
+      ticket = ["単勝,複勝", "馬連", "馬単", "ワイド", "3連複", "3連単"]
+      for i, name in enumerate(ticket):
+         if i < 5:
+            comp.append(Button(style=ButtonStyle.blue, label=name))
+         else:
+            comp2.append(Button(style=ButtonStyle.blue, label=name))
+
+      await ctx.send(
+          f"出走数は{starters}頭です。馬券の種類を選択してください",
+          components=[
+              comp,
+              comp2,
+          ],
+      )
+      while True:
+         interaction = await self.bot.wait_for("button_click")
+         print(interaction.user.display_name)
+         vote = self.quickpick_process(interaction.component.label, starters)
+         vote = f"{interaction.component.label}\n{vote}"
+         await interaction.respond(content=vote, ephemeral=True)
 
    def paste(self, img_list):
       img_width = 0
@@ -114,6 +165,26 @@ class Funny(commands.Cog):
                             avatar_url=ctx.message.author.avatar_url_as(format="png"))
          for i in sorted(glob.glob('picture/emojis/*png')):
             os.remove(i)
+
+   @commands.command("なおすきボタン")
+   async def naosuki_button(self, ctx, ch_id=None):
+      if ch_id is None:
+         channel = ctx.channel
+      else:
+         channel = self.bot.get_channel(744610643927236750)
+      await channel.send(
+          "まゆげ",
+          components=[
+              [Button(style=ButtonStyle.blue, label="なおすき")],
+          ],
+      )
+      while True:
+         interaction = await self.bot.wait_for("button_click")
+         # print(interaction.user.display_name)
+         url = 'https://discord.com/'
+         if interaction.component.label == "なおすき":
+            self.db.update('update naosuki_count set count=count+1')
+            await interaction.respond(content="なおすきカウントが増えた", ephemeral=True)
 
    @commands.command(aliases=["d"])
    async def stamp2(self, ctx, *emoji: discord.Emoji):
@@ -179,7 +250,7 @@ class Funny(commands.Cog):
             await message.add_reaction(i)
       if "なおすき" in message.content:
          self.db.update('update naosuki_count set count=count+1')
-         
+
 
 def setup(bot):
    bot.add_cog(Funny(bot))  # TestCogにBotを渡してインスタンス化し、Botにコグとして登録する。
